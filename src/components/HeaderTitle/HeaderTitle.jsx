ed3bd37da8439e5263f5699edf01cf5c125ecbc3
mdselimme <mdselimdev@gmail.com>
2024-11-05 18:38:47 +0600

    add_header_title

---
+++ src/components/HeaderTitle/HeaderTitle.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+
+const HeaderTitle = ({data}) => {
+    const {header_title,header_pargraph} = data;
+    return (
+        <div className="container mx-auto text-center mt-28 mb-9">
+            <h1 className="text-[#1A1919] text-5xl font-extrabold mb-7">{header_title}</h1>
+            <p className="text-[#757575] text-base font-medium leading-8">{header_pargraph}</p>
+        </div>
+    );
+};
+
+HeaderTitle.propTypes = {
+    data: PropTypes.object.isRequired
+}
+
+export default HeaderTitle;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,25 @@
 import Header from "../Header/Header";
+import HeaderTitle from "../HeaderTitle/HeaderTitle";
 import Banner from "./Banner/Banner";
 
 
 const Home = () => {
+
+    const commonTitleData = {
+        header_title:"Job Category List",
+        header_pargraph:"Explore diverse job categories ranging from technology and healthcare to marketing and finance, offering opportunities for every skill set."
+    }
+
     return (
-        <div className="bg-[#F9F9FF]">
-            <div className="container mx-auto">
-                <Header></Header>
-                <Banner></Banner>
-            </div>
-        </div>
+            <>
+                <div className="bg-[#F9F9FF]">
+                    <div className="container mx-auto">
+                        <Header></Header>
+                        <Banner></Banner>
+                    </div>
+                </div>
+                <HeaderTitle data={commonTitleData}></HeaderTitle>
+            </>
     );
 };
 
