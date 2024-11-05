febd0c7fce6766f8b2e50ba61746986156f9fcce
mdselimme <mdselimdev@gmail.com>
2024-11-05 11:09:48 +0600

    not_found_page_add

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
-
+import "./ErrorPage.css";
+import { IoHome } from "react-icons/io5";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <h2>Not Found</h2>
-            <Link to={"/"}><button className="btn btn-secondary">Secondary</button></Link>
+        <div className="error-page flex items-center justify-center">
+            <div>
+                <div className="w-[31.25rem] h-[31.25rem] bg-white mx-auto rounded-xl flex items-center justify-center flex-col">
+                    <h1 className="text-9xl font-extrabold text-center active">404</h1>
+                    <h3 className="text-3xl my-4 font-bold">Opps ! Page Not Found</h3>
+                    <Link to={"/"} className="flex items-center bg-common rounded-lg text-lg my-4"><IoHome className="mr-2" /> Return Home</Link>
+                </div>
+            </div>
         </div>
     );
 };
