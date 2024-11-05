b28a3fe1bfe38d7eb3bfdd9627b674432ced25fe
mdselimme <mdselimdev@gmail.com>
2024-11-05 11:21:17 +0600

    add_header_shadow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { LuLogIn } from "react-icons/lu";
 const Header = () => {
     return (
         <div className="container mx-auto py-8">
-            <div className="navbar bg-base-100 rounded-lg">
+            <div className="navbar bg-base-100 rounded-lg shadow">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
